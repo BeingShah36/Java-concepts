f8b24018c872db2fff48e3e03994b987ce5fcc08
BeingShah36 <shahbazalam.sayyad@gmail.com>
2024-10-26 15:31:30 +0530

    Add files via upload

---
+++ AbstractTuto.java
@@ -0,0 +1,12 @@
+
+public abstract class AbstractTuto {
+
+	public void EmirateRule() {
+		System.out.println("Rule");
+	}
+	public void EmirateDesign() {
+		System.out.println("Design");
+	}
+	
+	public abstract void Emiratecolor();
+}
